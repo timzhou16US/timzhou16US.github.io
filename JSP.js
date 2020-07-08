29b16400170c1dd2ab617d235155d665bf1bddf3
timzhou16US <51680608+timzhou16US@users.noreply.github.com>
2020-07-07 22:52:20 -0700

    Add files via upload

---
+++ JSP.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You changed your paragraph";
+}
+function changeitBack() {
+    document.getElementById("demo").innerHTML = "this is an example of a single quote. &lsquo; life is good &rsquo;";
+}
+function alertf() {
+    alert("open this link: https://us04web.zoom.us/j/74237384547?pwd=RjhkRVc0eXpqazdOZkxxVXcvRGJEUT09");
+}
+
+  function first() {
+  var person = {
+      firstname: "Tim",
+      lastname: "Zhou",
+      id: 5577
+  };
+  document.getElementById("first").innerHTML = person.firstname+" "+person.lastname;
+}
